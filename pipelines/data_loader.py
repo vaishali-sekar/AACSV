fab12f6733a0f025595e626f721def39dbcb4a87
vaishali-sekar <vaishalisekards@gmail.com>
2025-01-27 10:22:42 +0530

    Update data_loader.py

--- pipelines/data_loader.py
+++ pipelines/data_loader.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import pandas as pd
 
 def data_loader(file_path):
     print("Loading data...")
     data = pd.read_csv(file_path)
-=======
-import pandas as pd
-
-def data_loader(file_path):
-    print("Loading data...")
-    data = pd.read_csv(file_path)
->>>>>>> d7e64b796ce19bc24494cf120a3bbe8eb59697e3
     return data
