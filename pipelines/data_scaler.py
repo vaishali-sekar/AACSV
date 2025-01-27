2868436e85c495d1468c5f2688d3d57de816c723
vaishali-sekar <vaishalisekards@gmail.com>
2025-01-27 11:24:46 +0530

    Update data_scaler.py

--- pipelines/data_scaler.py
+++ pipelines/data_scaler.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import os
@@ -9,18 +9,4 @@ def data_scaler(X_train, X_test):
     
     X_train_scaled=scaler.fit_transform(X_train)
     X_test_scaled=scaler.transform(X_test)
-    
-=======
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-import os
-
-def data_scaler(X_train, X_test):
-
-    scaler=StandardScaler()
-    
-    X_train_scaled=scaler.fit_transform(X_train)
-    X_test_scaled=scaler.transform(X_test)
-    
->>>>>>> d7e64b796ce19bc24494cf120a3bbe8eb59697e3
     return X_train_scaled,X_test_scaled,scaler
