4a9fee09198ed5162522b68ac1ff5f51557c1ecb
vaishali-sekar <vaishalisekards@gmail.com>
2025-01-27 11:27:29 +0530

    Update train_test_split.py

--- pipelines/train_test_split.py
+++ pipelines/train_test_split.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
+
 from sklearn.model_selection import train_test_split
 
 def split_data(X, y, test_size=0.2, random_state=42):
     print("Splitting data into train and test sets...")
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
-=======
-from sklearn.model_selection import train_test_split
 
-def split_data(X, y, test_size=0.2, random_state=42):
-    print("Splitting data into train and test sets...")
-    return train_test_split(X, y, test_size=test_size, random_state=random_state)
->>>>>>> d7e64b796ce19bc24494cf120a3bbe8eb59697e3
